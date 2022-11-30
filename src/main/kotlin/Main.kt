8d9df53c444b798de500e57ab75997c6af35db4a
memoizr <memoizrlabs@gmail.com>
2022-11-30 20:58:05 +0100

    add Endpoint concept

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,3 +5,27 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+fun GET(path: String) = Endpoint(HTTPMethod.GET, path)
+
+data class Endpoint(
+    val httpMethod: HTTPMethod,
+    val url: String
+)
+
+enum class HTTPMethod {
+    GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD;
+
+    companion object {
+        fun fromString(method: String) = when (method) {
+            DELETE.name -> DELETE
+            GET.name -> GET
+            PUT.name -> PUT
+            POST.name -> POST
+            OPTIONS.name -> OPTIONS
+            HEAD.name -> HEAD
+            PATCH.name -> PATCH
+            else -> throw IllegalArgumentException(method)
+        }
+    }
+}
