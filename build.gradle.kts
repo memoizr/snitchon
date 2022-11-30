debbc2c478f7cffb928b00d60ea37dbbf38df0e5
memoizr <memoizrlabs@gmail.com>
2022-11-30 20:54:29 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+
+    kotlin("jvm") version "1.7.21"
+    application
+}
+
+group = "me.snitchon"
+version = "1.0-SNAPSHOT"
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers"
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
